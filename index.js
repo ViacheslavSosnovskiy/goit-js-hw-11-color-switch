7234eb645e38428a9649402b53822c28fc4b703b
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-08-03 21:40:43 +0300

    try doing hw

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ const colors = [
   "#795548",
 ];
 
+const startBtn = document.querySelector('[data-value="start"]');
+const stopBtn = document.querySelector('[data-value="stop"]');
+
+startBtn.addEventListener("click", onStartChangeColor);
+stopBtn.addEventListener("click", onStopChangeColor);
+
+function startChangeColor() {
+  console.log("start"); // ???
+}
+
+function stopChangeColor() {
+  console.log("stop"); // ???
+}
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+function changeColorBody() {
+  body.style.backgroundColor = colors; // ???
+}
